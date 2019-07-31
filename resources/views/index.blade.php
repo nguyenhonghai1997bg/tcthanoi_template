72517e378963667925a0828c5cf5c00bc8fdb1fc
nguyen.hong.hai <nguyen.hong.hai@i-enter.asia>
2019-07-31 10:47:46 +0700

    template: products in homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,19 +13,12 @@
                 <li><a href="#">Phụ kiện - Thiết bị số</a></li>
                 <li><a href="#">Đồ điện - Đồ gia dụng</a></li>
                 <li><a href="#">Thời trang nam nữ</a></li>
+                <li><a href="#">Phụ kiện - Thiết bị số</a></li>
+                <li><a href="#">Đồ điện - Đồ gia dụng</a></li>
+                <li><a href="#">Thời trang nam nữ</a></li>
             </ul>
         </div>
-        <div class="col-md-9 float-left mt-4 slide-show-image" style="width: 100%;">
-            <style>
-                #owl-demo .item img{
-                    display: block;
-                    width: 100%;
-                    height: auto;
-                }
-                .owl-carousel {
-                    width: 100%;
-                }
-            </style>
+        <div class="col-md-9 float-left mt-4 slide-show-image">
             <script src="{{ asset('js/libraries/owl.carousel.min.js') }}"></script>
             <div id="owl-demo" class="owl-carousel owl-theme">
                 <div class="item"><img src="https://lh4.googleusercontent.com/-EEOkrwIm4lw/WWw3xnYyN6I/AAAAAAAABOc/F2SazbfTOh8TGZmPiEQL-UISzT0JYqJfwCLcBGAs/s1600/img_coverage2.jpg" alt="The Last of us" width="100%"></div>
@@ -35,25 +28,50 @@
         </div>
         <div class="clearfix"></div>
     </div>
+
+    {{--content--}}
+    <div class="content mt-5">
+        <div class="box-products">
+            <h2 class="title-box">
+                <span>PHÂN BÓ LÁ</span>
+            </h2>
+            <a href="#" class="view-all">Xem tất cả</a>
+        </div>
+
+        <div class="products">
+            @for($i = 1; $i < 10; $i++)
+            <div class="product col-lg-3 col-md-3 col-sm-6 col-xs-6 float-left">
+                <div class="item">
+                    <div class="image">
+                        <img src="https://lh4.googleusercontent.com/-sJTpaNUY3X8/WWyC7MM3-bI/AAAAAAAABPg/T7zk0tC1QvEVg0zBY-iVGgBMGKgnUl7yQCLcBGAs/s1600/sp9.jpg">
+                    </div>
+                    <div class="product-content">
+                        <h3 class="title mt-4">Vitamin A Oil</h3>
+                        <div class="price">490,000₫</div>
+                        <div class="add-to-cart-btn mt-2">
+                            <div class="btn btn-success">Mua hàng</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endfor
+        </div>
+    </div>
 @endsection
 
 @section('script')
     <script>
         $(document).ready(function() {
             $("#owl-demo").owlCarousel({
-                navigation : true, // Show next and prev buttons
                 slideSpeed : 300,
                 paginationSpeed : 400,
                 singleItem:true,
                 loop: true,
                 items : 1,
-                itemsDesktop : false,
-                itemsDesktopSmall : false,
-                itemsTablet: false,
-                itemsMobile : false,
                 dots: true,
                 autoplay: true,
-
+                nav: true,
+                navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
             });
         });
     </script>
